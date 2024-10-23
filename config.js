0714a61b4c4a4e00bce108684e77d53aaf2dffd1
Loukson <123784461+Loukson@users.noreply.github.com>
2024-10-23 01:08:04 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "7DR12LxL#L8ggt4p0ITLuQFCaPvfso9VaVURJwYX_dYRREqCFkIU",
 MONGODB: process.env.MONGODB || "",
 };
